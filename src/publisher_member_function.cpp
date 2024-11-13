6a73d73e21127e46d7a361a16d3ec5cef9d2b0ae
Amogha Sunil <amoghats@umd.edu>
2024-11-13 12:41:07 -0500

    TASK: Modified Talker node to broadcast a tf frame /talk with /world

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,7 +1,7 @@
 /**
  * BSD 3-Clause License
  * @file publisher_member_function.cpp
- * @brief Publisher node and service using callback
+ * @brief Publisher node and service using callback, with tf broadcaster
  * @version 1.0
  * @date 2024-11-07
  * @author Amogha Sunil<amoghats@umd.edu>
@@ -16,6 +16,9 @@
 #include "rclcpp/logger.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "rclcpp/parameter_client.hpp"
 
 /*Parameter Types */
 using PARAMETER_EVENT = std::shared_ptr<rclcpp::ParameterEventHandler>;
@@ -24,7 +27,7 @@ using PARAMETER_HANDLE = std::shared_ptr<rclcpp::ParameterCallbackHandle>;
 /**
  * @class MinimalPublisher
  * @brief A publisher ros2 node which publishes messages to a topic and provides
- * a service to change the message content.
+ * a service to change the message content, along with broadcasting a tf frame.
  */
 class MinimalPublisher : public rclcpp::Node {
  public:
@@ -35,6 +38,7 @@ class MinimalPublisher : public rclcpp::Node {
   MinimalPublisher() : Node("Minimal_Custom_Publisher"), count(0) {
     try {
       publisher = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      tfBroadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
       auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
 
@@ -93,17 +97,43 @@ class MinimalPublisher : public rclcpp::Node {
     auto message = std_msgs::msg::String();
     message.data = service_response_message;
 
-    // Log the message data
+    ///<  Log the message data
     RCLCPP_DEBUG_STREAM(this->get_logger(), " Inserting the message data");
 
-    // Publish the message
+    ///<  Publish the message
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher->publish(message);
+    
+    ////<  Broadcast transform
+    geometry_msgs::msg::TransformStamped transformStamped;
+
+    ///< Set header and parent frame
+    transformStamped.header.stamp = this->get_clock()->now();
+    transformStamped.header.frame_id = "world";
+    transformStamped.child_frame_id = "talk";
+
+    ///< translation (non-zero values)
+    transformStamped.transform.translation.x = 1.0;
+    transformStamped.transform.translation.y = 2.0;
+    transformStamped.transform.translation.z = 3.0;
+
+    ///< rotation (non-zero quaternion)
+    transformStamped.transform.rotation.x = 0.0;
+    transformStamped.transform.rotation.y = 0.0;
+    ///< 45 degrees in z axis
+    transformStamped.transform.rotation.z = 0.707;  
+    transformStamped.transform.rotation.w = 0.707;
+
+    ///<  Broadcast the transform
+    tfBroadcaster->sendTransform(transformStamped);
   }
   rclcpp::TimerBase::SharedPtr timer;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
   ///< Service object to handle service requests.
   rclcpp::Service<beginner_tutorials::srv::ChangeString>::SharedPtr service;
+
+  ///< tf broadcaster object 
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tfBroadcaster;
 
   ///< Message to be published, updated by the service.
   std::string service_response_message{" Hi, from Amogha !"};
